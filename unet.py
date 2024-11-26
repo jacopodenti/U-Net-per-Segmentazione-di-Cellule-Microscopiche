89ac124ad3e442aba3baf9ad9f7f6d01fc145193
Paolo Sangalli <paolo.sangalli1@studenti.unimi.it>
2024-11-26 17:13:01 +0100

    commit inutile

--- unet.py
+++ unet.py
@@ -31,17 +31,17 @@ def simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classe
     c2 = Dropout(0.1)(c2)
     c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c2)
     p2 = MaxPooling3D((2, 2, 2))(c2)
-     
+    
     c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p2)
     c3 = Dropout(0.2)(c3)
     c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c3)
     p3 = MaxPooling3D((2, 2, 2))(c3)
-     
+
     c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p3)
     c4 = Dropout(0.2)(c4)
     c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c4)
     p4 = MaxPooling3D(pool_size=(2, 2, 2))(c4)
-     
+
     c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p4)
     c5 = Dropout(0.3)(c5)
     c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c5)
@@ -52,27 +52,27 @@ def simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classe
     c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u6)
     c6 = Dropout(0.2)(c6)
     c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c6)
-     
+
     u7 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(c6)
     u7 = concatenate([u7, c3])
     c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u7)
     c7 = Dropout(0.2)(c7)
     c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c7)
-     
+
     u8 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(c7)
     u8 = concatenate([u8, c2])
     c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u8)
     c8 = Dropout(0.1)(c8)
     c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c8)
-     
+
     u9 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same')(c8)
     u9 = concatenate([u9, c1])
     c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u9)
     c9 = Dropout(0.1)(c9)
     c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c9)
-     
+
     outputs = Conv3D(num_classes, (1, 1, 1), activation='softmax')(c9)
-     
+    
     model = Model(inputs=[inputs], outputs=[outputs])
     #compile model outside of this function to make it flexible. 
     model.summary()
