45581a03ed7141f262f77b2df8af7413cb9bd5fa
Paolo Sangalli <paolo.sangalli1@studenti.unimi.it>
2024-11-27 16:47:03 +0100

    pullami

--- unet.py
+++ unet.py
@@ -4,6 +4,17 @@ from tensorflow.keras import layers, models
 from PIL import Image, UnidentifiedImageError
 import numpy as np
 import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Paths to the data
+PERCORSO_TRAINING_IMMAGINI = os.getenv('PERCORSO_TRAINING_LABELED')
+PERCORSO_TRAINING_LABELS = os.getenv('PERCORSO_TRAINING_LABELED_LABELS')
+PERCORSO_TUNING_IMMAGINI = os.getenv('PERCORSO_TUNING')
+PERCORSO_TUNING_LABELS = os.getenv('PERCORSO_TUNING_LABELS')
+PERCORSO_OUTPUT = os.getenv('PERCORSO_OUTPUT')
 
 # Funzione per costruire il modello UNet
 def unet_model(input_size=(256, 256, 1)):
@@ -93,13 +104,6 @@ def load_dataset(image_dir, label_dir):
     print(f"Numero di etichette caricate: {len(labels)}")
     assert len(images) == len(labels), "Il numero di immagini e etichette non corrisponde."
     return tf.data.Dataset.from_tensor_slices((images, labels))
-
-# Imposta i percorsi direttamente nel codice
-PERCORSO_TRAINING_IMMAGINI = '/Users/utente/Downloads/Training-labeled/images'
-PERCORSO_TRAINING_LABELS = '/Users/utente/Downloads/Training-labeled/labels'
-PERCORSO_TUNING_IMMAGINI = '/Users/utente/Downloads/Tuning/images'
-PERCORSO_TUNING_LABELS = '/Users/utente/Downloads/Tuning/labels'
-PERCORSO_OUTPUT = '/Users/utente/Desktop/output-principi'
 
 # Carica i dataset di addestramento e tuning
 train_dataset = load_dataset(PERCORSO_TRAINING_IMMAGINI, PERCORSO_TRAINING_LABELS)
